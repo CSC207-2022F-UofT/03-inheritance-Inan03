b9ee867bdaaa6da087f7ca51dc96353e2be4ef66
Inan03 <sanon.inan@gmail.com>
2022-10-09 11:05:38 -0400

    Completed

--- src/main/java/HandBag.java
+++ src/main/java/HandBag.java
@@ -30,7 +30,6 @@ public class HandBag extends Bag{
         /* TODO: Implement this method.
          *       You may want to use the increaseCapacity() method that was
          *       implemented in Bag.
-         *
          * To call a method defined in a parent, you use super.method_name(...)
          */
         super.increaseCapacity(1);
